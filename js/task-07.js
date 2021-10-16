6e0a549943fb99ff63f8c380dcbc84a0bfdc7747
Julia-Tuschenko <juliayulianna@gmail.com>
2021-10-16 15:19:18 +0300

    7

--- js/task-07.js
+++ js/task-07.js
@@ -1,15 +1,38 @@
-const inputForm = document.querySelector("#font-size-control");
-const textRef = document.querySelector("#text");
+const fondSizeControl = document.querySelector('#font-size-control');
+const textSpan = document.querySelector('#text');
 
+fondSizeControl.value = Number.parseInt(window.getComputedStyle(textSpan).fontSize);
 
+fondSizeControl.addEventListener('input', onInputChangeText);
 
-inputForm.addEventListener('input', (elem) => {
-    // elem = inputForm .getAttribute('range');
-    textRef.style.fontSize = parseInt(elem.currentTarget.value);
+function onInputChangeText(event){
+    textSpan.style.fontSize = event.currentTarget.value + 'px' ;
 }
-);
 
-console.log(inputForm);
+
+
+
+
+
+
+// const fontSizeControl = document.querySelector("#font-size-control");
+// const textRef = document.querySelector("#text");
+// fontSizeControl.value = Number.parseInt(window.getComputedStyle(textRef).fontSize);
+
+// fontSizeControl.addEventListener('input', onInputChangeText);
+
+// function onInputChangeText(event){
+//     textRef.style.fontSize = event.currentTarget.velue + `px`;
+// }
+
+
+// inputForm.addEventListener('input', (elem) => {
+//     // elem = inputForm .getAttribute('range');
+//     textRef.style.fontSize = parseInt(elem.currentTarget.value);
+// }
+// );
+
+// console.log(inputForm);
 
 // function textRefFontSize(size){
 //     textRefNumber = parseInt(size.value);
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,38 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+
+
+function onFormSubmit(event){
+event.preventDefault();
+const formElements = event.currentTarget.elements;
+
+const email = formElements.email.value;
+const password = formElements.password.value;
+
+const formDate = {
+    email,
+    password,
+};
+
+if(email === `` || password === ``){
+    alertText(formDate);
+}
+console.log(formDate);
+
+formFinish();
+}
+
+function alertText (){
+    window.alert(`Всі поля повинні бути заповнені`);
+};
+
+function formFinish(){
+    document.getElementById(form).requestFullscreen();
+}
+
+
+
+// window.alert(`Всі поля повинні бути заповнені`)
+// const formData = new FormData(event.currentTarget);
