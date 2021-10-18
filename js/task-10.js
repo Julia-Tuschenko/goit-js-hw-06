0c0efe0ec57fc4e3ac9a0425a69964873ebf4bed
Julia Tushchenko <juliayulianna@gmail.com>
2021-10-18 16:33:45 +0300

    10

--- js/task-10.js
+++ js/task-10.js
@@ -1,27 +1,95 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// const refs = {
+//   controlsEl: document.querySelector('#controls'),
+//   btnCreate: document.querySelector('button[data-create]'),
+//   btnDestroy: document.querySelector('button[data-destroy]'),
+//   boxes: document.querySelector('#boxes'),
+
+// }
+// refs.btnCreate.addEventListener('click', onCollection)
+
+// function onCollection(evt){
+//   const  divConteiner = [evt];
+
+//   const imagesRef  = divConteiner
+//   .map(element => 
+//     `<div class="div" ${element}></div>`)
+//   .join("");
+
+//   return imagesRef;
+// };
+
+const render = document.querySelector('button[data-create]');
+const destroy = document.querySelector('button[data-destroy]');
+const boxes = document.getElementById("#boxes");
+
+render.addEventListener("click", getAmount);
+destroy.addEventListener("click", destroyBoxes);
+
+function getAmount() {
+  var amount = +document.querySelector("#controls input").value;
+  createBoxes(amount);
 }
 
-const refs = {
-  controlsEl: document.querySelector('#controls'),
-  btnCreate: document.querySelector('button[data-create]'),
-  btnDestroy: document.querySelector('button[data-destroy]'),
-  boxes: document.querySelector('#boxes'),
-
+function createBoxes(amount) {
+  var basicSize = 30;
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < amount; i++) {
+    var size = basicSize + i * 10;
+    var div = document.createElement("div");
+    div.style.cssText = `width: ${size}px; height: ${size}px; background-color: rgba( ${random()} , ${random()} , ${random()} )`;
+    fragment.appendChild(div);
+  }
+  boxes.appendChild(fragment);
 }
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
+function random() {
+  return Math.floor(Math.random() * 256);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // console.log(refs);
 //  const mySet = new Set();
 
-refs.btnCreate.addEventListener('click', ready);
+// refs.btnCreate.addEventListener('click', ready);
 
-const amount=0 ;
+// const amount=0 ;
 
-$(document).ready(function() {
-    for(const i = 1; i <= amount; i++) {
-      const div = document.getElementById('test');
-        div.innerHTML = div.innerHTML + '<div id="boxes' + i + ' class="div">new divs</ div>';
-     } 
-});
+// $(document).ready(function() {
+//     for(const i = 1; i <= amount; i++) {
+//       const div = document.getElementById('test');
+//         div.innerHTML = div.innerHTML + '<div id="boxes' + i + ' class="div">new divs</ div>';
+//      } 
+// });
 
 
 
