1a4c7f669f939fc19efc81b5a1b421169792b636
Julia-Tuschenko <juliayulianna@gmail.com>
2021-10-18 21:51:02 +0300

    10

--- js/task-10.js
+++ js/task-10.js
@@ -1,6 +1,50 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const ref = {
+  number: document.querySelector("input"),
+  btCreate: document.querySelector("[data-create]"),
+  btDestroy: document.querySelector("[data-destroy]"),
+  boxes: document.querySelector("#boxes"),
+ }
+ 
+ const INTIAL_BOX_SIZE = 30;
+ const BOX_SIZE_STEP = 10;
+ 
+ let boxSize = INTIAL_BOX_SIZE;
+ 
+ function createBox(amount) {
+   for (let i = 1; i <= amount; i += 1) {
+     const box = `<div style="width: ${boxSize}px; height: ${boxSize}px; background-color:${getRandomHexColor()};"></div>`;
+     ref.boxes.insertAdjacentHTML('beforeend', box);
+     boxSize += BOX_SIZE_STEP;
+   }
+ }
+ 
+ function resetBoxSize() {
+   boxSize = INTIAL_BOX_SIZE;
+ }
+ 
+ function destroyBox() {
+    ref.boxes.innerHTML = '';
+ };
+ 
+ ref.btCreate.addEventListener("click", () => {
+   createBox( ref.number.value);
+ });
+ console.log();
+ ref.btDestroy.addEventListener("click", () => {
+   destroyBox();
+    ref.number.value = "";
+ });
+
+
+
+
+
+
+
 
 // const refs = {
 //   controlsEl: document.querySelector('#controls'),
@@ -22,37 +66,37 @@
 //   return imagesRef;
 // };
 
-const render = document.querySelector('button[data-create]');
-const destroy = document.querySelector('button[data-destroy]');
-const boxes = document.getElementById("#boxes");
+// const render = document.querySelector('button[data-create]');
+// const destroy = document.querySelector('button[data-destroy]');
+// const boxes = document.getElementById("#boxes");
 
-render.addEventListener("click", getAmount);
-destroy.addEventListener("click", destroyBoxes);
+// render.addEventListener("click", getAmount);
+// destroy.addEventListener("click", destroyBoxes);
 
-function getAmount() {
-  var amount = +document.querySelector("#controls input").value;
-  createBoxes(amount);
-}
+// function getAmount() {
+//   var amount = +document.querySelector("#controls input").value;
+//   createBoxes(amount);
+// }
 
-function createBoxes(amount) {
-  var basicSize = 30;
-  var fragment = document.createDocumentFragment();
-  for (var i = 0; i < amount; i++) {
-    var size = basicSize + i * 10;
-    var div = document.createElement("div");
-    div.style.cssText = `width: ${size}px; height: ${size}px; background-color: rgba( ${random()} , ${random()} , ${random()} )`;
-    fragment.appendChild(div);
-  }
-  boxes.appendChild(fragment);
-}
+// function createBoxes(amount) {
+//   var basicSize = 30;
+//   var fragment = document.createDocumentFragment();
+//   for (var i = 0; i < amount; i++) {
+//     var size = basicSize + i * 10;
+//     var div = document.createElement("div");
+//     div.style.cssText = `width: ${size}px; height: ${size}px; background-color: rgba( ${random()} , ${random()} , ${random()} )`;
+//     fragment.appendChild(div);
+//   }
+//   boxes.appendChild(fragment);
+// }
 
-function destroyBoxes() {
-  boxes.innerHTML = "";
-}
+// function destroyBoxes() {
+//   boxes.innerHTML = "";
+// }
 
-function random() {
-  return Math.floor(Math.random() * 256);
-}
+// function random() {
+//   return Math.floor(Math.random() * 256);
+// }
 
 
 
